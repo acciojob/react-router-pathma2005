3580258240effea7a4ec98263fea07155c55bc56
Pathmasri E <pathmasri1372005@gmail.com>
2025-12-07 18:53:54 +0530

    Add About component with sample content

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+function About(){
+    return(
+        <div>
+            <h2>About</h2>
+            <p>This is a sample React Router program.</p>
+        </div>
+    );
+}
+export default About;
